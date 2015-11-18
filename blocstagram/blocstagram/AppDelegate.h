91a43f648042d6596b0ed3b54d0a869c15aeab4e
YooJ <yoojinkimhong@gmail.com>
2015-11-17 17:45:24 -0800

    this beter work

---
+++ blocstagram/blocstagram/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  blocstagram
+//
+//  Created by YooJin Hong on 11/17/15.
+//  Copyright © 2015 YooJin Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ blocstagram/blocstagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  blocstagram
+//
+//  Created by YooJin Hong on 11/17/15.
+//  Copyright © 2015 YooJin Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
