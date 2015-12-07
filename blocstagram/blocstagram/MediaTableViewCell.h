3139069725a7b28a207552a3e0882a74703b9dd6
YooJ <yoojinkimhong@gmail.com>
2015-12-06 21:10:00 -0800

    added media table view cell

---
+++ blocstagram/blocstagram/MediaTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MediaTableViewCell.h
+//  blocstagram
+//
+//  Created by YooJin Hong on 12/6/15.
+//  Copyright © 2015 YooJin Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MediaTableViewCell : UITableViewCell
+
+@end
---
+++ blocstagram/blocstagram/MediaTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MediaTableViewCell.m
+//  blocstagram
+//
+//  Created by YooJin Hong on 12/6/15.
+//  Copyright © 2015 YooJin Hong. All rights reserved.
+//
+
+#import "MediaTableViewCell.h"
+
+@implementation MediaTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
