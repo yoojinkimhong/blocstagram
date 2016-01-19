4faa409d53a9a5481169461cbe562fcf5c2e5ff2
YooJ <yoojinkimhong@gmail.com>
2016-01-18 19:00:22 -0800

    completed login checkpoint

---
+++ blocstagram/blocstagram/LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  blocstagram
+//
+//  Created by YooJin Hong on 1/13/16.
+//  Copyright © 2016 YooJin Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+extern NSString *const LoginViewControllerDidGetAccessTokenNotification;
+
+@end
