b6cd145a4bf1ee7cbf91866abb6afd1ccce60dc1
YooJ <yoojinkimhong@gmail.com>
2016-01-21 17:47:14 -0800

    work in progress blocstagram-fullscreen checkpoint

---
+++ blocstagram/blocstagram/MediaFullScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  MediaFullScreenViewController.h
+//  blocstagram
+//
+//  Created by YooJin Hong on 1/20/16.
+//  Copyright © 2016 YooJin Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MediaFullScreenViewController : UIViewController
+
+@end
